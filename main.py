f5420f9a619c8849edc87ab3503c88a3b369ce2a
saifdevx <saifdevcore@gmail.com>
2026-07-06 16:41:56 +0500

    Add Pydantic schemas for request validation and response filtering

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
+from schemas import PostCreate , PostResponse
+
+
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
@@ -96,13 +100,33 @@ def get_post_page(request : Request , post_id : int):
 
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
 
 
+## Create Post
+@app.post(
+    "/api/posts",
+    response_model=PostResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025",
+    }
+    posts.append(new_post)
+    return new_post
 
-@app.get("/api/posts/{post_id}")
+
+
+
+@app.get("/api/posts/{post_id}" , response_model=PostResponse)
 def get_post(post_id : int):
     for post in posts:
         if post.get("id") == post_id:
